55b7df7cec4168d44369f5ff064e0eb1bdabde8c
Muluneh92Petros <mulunehpetros89@gmail.com>
2024-10-27 05:17:53 -0700

    script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -55,7 +55,7 @@ function loadLastViewedQuote() {
 }
 loadLastViewedQuote();
 let quotes = [
-    { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Inspiration" },
+    { text: ["method", "POST", "headers", "Content-Type"] },
     { text: "Life is 10% what happens to us and 90% how we react to it.", category: "Motivation" },
     { text: "Your time is limited, don't waste it living someone else's life.", category: "Life" }
 ];
